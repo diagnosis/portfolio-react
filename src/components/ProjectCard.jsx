eedb5a477dba2f196af9f21ada2489915ec04485
safa demirkan <demirkansafa@gmail.com>
2025-05-22 16:45:10 -0700

    Update ProjectCard to use new Modal implementation

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
-import { Modal } from './Modal';
+import { Modal } from '../Modal';
 import { useState } from 'react';
 
 export const ProjectCard = ({ project }) => {
@@ -40,31 +40,39 @@ export const ProjectCard = ({ project }) => {
                 </div>
             </div>
 
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-                <div className="space-y-4">
-                    <h3 className="text-2xl font-heading text-teal-100">{project.name}</h3>
-                    <p className="text-indigo-100">{project.description}</p>
-                    
-                    <h4 className="text-xl font-heading text-teal-100 mt-6">Responsibilities</h4>
-                    <ul className='list-disc list-inside space-y-2 text-indigo-100'>
-                        {project.responsibilities.map((item, index) => (
-                            <li key={`${project.name}-resp-${index}`} className='text-sm'>{item}</li>
-                        ))}
-                    </ul>
-                    
-                    <h4 className="text-xl font-heading text-teal-100 mt-6">Technologies</h4>
-                    <div className='flex flex-wrap gap-2'>
-                        {project.technologies?.map((tech, index) => (
-                            <span 
-                                key={`${project.name}-tech-${index}`}
-                                className='px-2 py-1 text-xs rounded-full bg-teal-500/20 text-teal-100'
-                            >
-                                {tech}
-                            </span>
-                        ))}
+            {isModalOpen && (
+                <Modal>
+                    <div className="space-y-4 text-white">
+                        <h3 className="text-2xl font-heading text-teal-100">{project.name}</h3>
+                        <p className="text-indigo-100">{project.description}</p>
+                        
+                        <h4 className="text-xl font-heading text-teal-100 mt-6">Responsibilities</h4>
+                        <ul className='list-disc list-inside space-y-2 text-indigo-100'>
+                            {project.responsibilities.map((item, index) => (
+                                <li key={`${project.name}-resp-${index}`} className='text-sm'>{item}</li>
+                            ))}
+                        </ul>
+                        
+                        <h4 className="text-xl font-heading text-teal-100 mt-6">Technologies</h4>
+                        <div className='flex flex-wrap gap-2'>
+                            {project.technologies?.map((tech, index) => (
+                                <span 
+                                    key={`${project.name}-tech-${index}`}
+                                    className='px-2 py-1 text-xs rounded-full bg-teal-500/20 text-teal-100'
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                        <button
+                            onClick={() => setIsModalOpen(false)}
+                            className="mt-4 px-4 py-2 bg-teal-500/20 text-teal-100 rounded-lg hover:bg-teal-500/30 transition-colors duration-200"
+                        >
+                            Close
+                        </button>
                     </div>
-                </div>
-            </Modal>
+                </Modal>
+            )}
         </>
     );
 };
