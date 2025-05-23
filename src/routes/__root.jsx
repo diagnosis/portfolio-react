4c37204535c8713980160750e27cfcdaef4394e8
safa demirkan <demirkansafa@gmail.com>
2025-05-23 05:55:56 -0700

    Fix layout scrolling

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -3,17 +3,18 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Header } from '../Header.jsx';
 import Footer from "../Footer.jsx";
 
-
 export const Route = createRootRoute({
     component: RootComponent,
 });
 
 function RootComponent() {
     return (
-        <div className="min-h-screen">
+        <div className="flex flex-col min-h-screen">
             <Header />
+            <main className="flex-1 overflow-auto">
                 <Outlet />
-            <Footer></Footer>
+            </main>
+            <Footer />
             <TanStackRouterDevtools />
         </div>
     );
