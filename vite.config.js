58bc33c4d55a956020752e071a42741a9f6392a3
diagnosis <demirkansafa@gmail.com>
2025-05-23 05:53:42 -0700

    removing frost bite

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import TanStackRouterVite from "@tanstack/router-plugin/vite";
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({
     target: 'react',
     autoCodeSplitting: true,
-  }), react(), tailwindcss(), flowbiteReact()],
+  }), react(), tailwindcss()],
 })
