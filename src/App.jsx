42b74a20d6483c80e1d604de6a270180d3370277
diagnosis <demirkansafa@gmail.com>
2025-05-24 03:51:39 -0700

    This rs commit 9f44cdf783230ba620d9773cb1b0c245e3ad5388.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { HelmetProvider } from 'react-helmet-async'
+
 import { routeTree } from './routeTree.gen'
+
 
 const root = createRoot(document.getElementById('root'))
 const router = createRouter({routeTree})
 
-const App = () => {
+const App = ()=>{
     return (
-        <HelmetProvider>
             <RouterProvider router={router}/>
-        </HelmetProvider>
     )
 }
 
